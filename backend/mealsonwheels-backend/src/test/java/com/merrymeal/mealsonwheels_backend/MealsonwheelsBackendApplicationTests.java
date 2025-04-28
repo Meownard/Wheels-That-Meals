7dd3d763f43b5abf3b41d6833841cba62a0d107a
Meownard <menard.luya@gmail.com>
2025-04-28 18:01:06 +0800

    Initial commit

---
+++ backend/mealsonwheels-backend/src/test/java/com/merrymeal/mealsonwheels_backend/MealsonwheelsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.merrymeal.mealsonwheels_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealsonwheelsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
